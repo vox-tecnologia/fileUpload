9ed76a575453b89cb871964a2c96abb7d3786bcb
jose.ferreira <jose@voxtecnologia.com.br>
2018-05-08 14:30:50 -0300

    + anotando classes para compodoc + ajustado README com informações sobre uso do

--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -4,7 +4,13 @@ import { FileItem, FileUploader } from 'ng2-file-upload';
 
 import { TypesEnum } from '../enum/types.enum';
 
-
+/**
+ *
+ * Class que gerência o component de Upload
+ * @export
+ * @class UploadComponent
+ * @implements {OnInit}
+ */
 @Component({
   selector: 'app-vox-upload',
   templateUrl: './upload.component.html',
@@ -26,6 +32,10 @@ export class UploadComponent implements OnInit {
   public isUp: Array<boolean>;
   public actions: Object;
 
+  /**
+   * Creates an instance of UploadComponent.
+   * @memberof UploadComponent
+   */
   constructor() {
     this._limtSize = 2;  // limite maximo para anexos de arquivos
     this.maxSize = 2;
@@ -35,6 +45,12 @@ export class UploadComponent implements OnInit {
     this.alerts = { status: false, msgs: [] };
   }
 
+  /**
+   *
+   * Método pertencente ao Lifecycle hook, responsável pela inicialização do componente
+   * @returns {void}
+   * @memberof UploadComponent
+   */
   ngOnInit(): void {
     this.uploader = new FileUploader({
       url: this.url
@@ -71,10 +87,16 @@ export class UploadComponent implements OnInit {
 
   }
 
+  /**
+   * Método responsável por executar os metódos do FileUploader
+   * @returns {void}
+   * @memberof UploadComponent
+   */
   public initFile(): void {
     this.uploader.onAfterAddingFile = (item: FileItem) => {
 
       item.withCredentials = false;
+
       if (!this.isvalidarSize(item)) {
         item.remove();
         const err = `Error: (Tamanho) O <strong>${item.file.name}</strong> possui tamanho de
@@ -121,6 +143,14 @@ export class UploadComponent implements OnInit {
 
   }
 
+  /**
+   *
+   * Método responsável por validar o tamanho dos arquivos
+   * @private
+   * @param {FileItem} item
+   * @returns {boolean}
+   * @memberof UploadComponent
+   */
   private isvalidarSize(item: FileItem): boolean {
 
     const fileSizeinMB = item.file.size / (1024 * 1000);
@@ -133,6 +163,14 @@ export class UploadComponent implements OnInit {
     return true;
   }
 
+  /**
+   *
+   * Método responsável por validar o tipo de extensão dos arquivos
+   * @private
+   * @param {FileItem} item
+   * @returns {boolean}
+   * @memberof UploadComponent
+   */
   private isValidaType(item: FileItem): boolean {
 
     const type = item.file.name.split('.');
@@ -152,10 +190,24 @@ export class UploadComponent implements OnInit {
 
   }
 
+  /**
+   *
+   * Método responsável por retorna um lista com os tipos de extensões permitidas
+   * @private
+   * @returns {Array<string>}
+   * @memberof UploadComponent
+   */
   private extension(): Array<string> {
     return this._extensoes = [TypesEnum.PDF, TypesEnum.PNG, TypesEnum.JPEG, TypesEnum.JPG, TypesEnum.CSV];
   }
 
+  /**
+   *
+   * Método responsável por exibir as extensões validas nas mensagens
+   * @private
+   * @returns {string}
+   * @memberof UploadComponent
+   */
   private msgExtension(): string {
     if (this.fileExt) {
       return this.fileExt.toString().toLocaleUpperCase();
@@ -164,10 +216,24 @@ export class UploadComponent implements OnInit {
     return this.extension().toString().toLocaleUpperCase();
   }
 
+  /**
+   *
+   * Método responsável por informa a quantidade de arquivos na Queue[]
+   * @param {Array<FileItem>} item
+   * @returns {(number | string)}
+   * @memberof UploadComponent
+   */
   public fileQtd(item: Array<FileItem>): number | string {
     return item.length === 0 ? 'Não há arquivos anexados' : item.length;
   }
 
+  /**
+   *
+   * Método responsável por informa o tamanho do arquivos em 'Bytes', 'KB', 'MB', 'GB'
+   * @param {any} bytes
+   * @returns {string}
+   * @memberof UploadComponent
+   */
   public formateSize(bytes): string {
 
     const sizes = ['Bytes', 'KB', 'MB', 'GB'];
@@ -189,34 +255,77 @@ export class UploadComponent implements OnInit {
 
   }
 
+  /**
+   *
+   * Método responsável por montar a tabela com os items para upload
+   * @returns {Array<any>}
+   * @memberof UploadComponent
+   */
   public getAnexos(): Array<any> {
     return this.anexosRequeridos;
   }
 
+  /**
+   * Método responsável por exibir o icone do anexo
+   * @param {string} icon
+   * @returns {Array<string>}
+   * @memberof UploadComponent
+   */
   public icon(icon: string): Array<string> {
       return this.extension().filter(ext => {
         return icon === ext;
       });
   }
 
+  /**
+   *
+   * Método responsável verificar qual item da tabela foi selecionado
+   * @param {number} anexoId
+   * @param {number} idx
+   * @returns {void}
+   * @memberof UploadComponent
+   */
   public changeRow(anexoId: number, idx: number): void {
     this.rows[idx] = anexoId;
     this.isUp[idx] = true;
   }
 
+  /**
+   * Método responsável por enviar todos os arquivos na Queue[]
+   * @returns {void}
+   * @memberof UploadComponent
+   */
   public uploadAll(): void {
     this.uploader.uploadAll();
   }
 
+  /**
+   * Método responsável por remover todos os arquivos da Queue[]
+   * @returns {void}
+   * @memberof UploadComponent
+   */
   public clearQueue(): void {
     this.uploader.clearQueue();
     this.clearTextOptions();
   }
 
+  /**
+   *
+   * Método responsável por alterna o texto do botão selecionar/alterar
+   * @private
+   * @returns {void}
+   * @memberof UploadComponent
+   */
   private clearTextOptions(): void {
     this.isUp = this.isUp.map(ele => ele = false);
   }
 
+  /**
+   *
+   *  Método responsável por exibir os status das requisições
+   * @returns {Object}
+   * @memberof UploadComponent
+   */
   public getAlert(): Object {
     return {
         'alert-success' : true === this.alerts['status'],
